b6661b0bd233238c3f54c7eb10e7272ed8f0e9a2
NoMaL21 <dntjd8509@naver.com>
2024-08-19 14:58:01 +0900

    파라미터 명시화와 @PathVariable과 @RequestParam의 사용

--- demo/src/main/java/com/example/demo/TestController.java
+++ demo/src/main/java/com/example/demo/TestController.java
@@ -3,7 +3,8 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("test")
@@ -11,5 +12,21 @@ public class TestController {
 	@GetMapping
 	public String testController() {
 		return "Hello World";
+	}
+	
+	@GetMapping("/{id}")
+	public String testControllerWithPathVariables(@PathVariable(name = "id", required = false) Integer id) {
+	    if (id == null) {
+	        return "Hello World! ID is not provided";
+	    }
+	    return "Hello World! ID " + id;
+	}
+
+	@GetMapping("/Param")
+	public String testControllerRequestParam(@RequestParam(name = "id", required = false) Integer id) {
+	    if (id == null) {
+	        return "Hello World! ID param is not provided";
+	    }
+	    return "Hello World! ID param " + id;
 	}
 }
