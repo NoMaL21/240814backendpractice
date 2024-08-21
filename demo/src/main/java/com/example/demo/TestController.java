ca6c621cffc2e080cc422d6a29e78cbbbd5587bb
NoMaL21 <dntjd8509@naver.com>
2024-08-21 19:18:02 +0900

    @Service를 사용해 비즈니스 데이터 출력

--- demo/src/main/java/com/example/demo/TestController.java
+++ demo/src/main/java/com/example/demo/TestController.java
@@ -3,23 +3,27 @@ package com.example.demo;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.print.DocFlavor.STRING;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-
-
 
 import com.example.demo.dto.TestRequestBodyDTO;
 import com.example.demo.dto.ResponseDTO;
+import com.example.demo.service.TodoService;
 
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
-@RequestMapping("test")
+//@RequestMapping("test")
+@RequestMapping("todo")
 public class TestController {
+	/*
 	@GetMapping
 	public String testController() {
 		return "Hello World";
@@ -72,6 +76,19 @@ public class TestController {
 		list.add("See you!");
 		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
 		return ResponseEntity.badRequest().body(response);
+	}
+	*/
+	
+	@Autowired
+	private TodoService service;
+	
+	@GetMapping("/test")
+	public ResponseEntity<?>testTodo(){
+		String str = service.testService();
+		List<String> list = new ArrayList<>();
+		list.add(str);
+		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+		return ResponseEntity.ok().body(response);
 	}
 	
 }
